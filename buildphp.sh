6cadca11999b4793c4ec500aa67598e236431073
Chris Hamper <chris.hamper@acquia.com>
2018-09-27 14:01:04 -0400

    Specify PHP version to build as command line argument

--- buildphp.sh
+++ buildphp.sh
@@ -6,7 +6,17 @@
 # You can specify the PHP Version by setting the branch corresponding to the
 # source from https://github.com/php/php-src
 
-PHP_VERSION_GIT_BRANCH=php-7.2.0
+if [ $# -lt 1 ]; then
+  echo
+  echo "Usage: $0 VERSION"
+  echo "Build shared libraries for php and its dependencies via containers"
+  echo
+  echo "Please specify the php VERSION, e.g. 7.1.5, 7.0.20, 5.6.30"
+  echo
+  exit 1
+fi
+
+PHP_VERSION_GIT_BRANCH="php-$1"
 
 echo "Build PHP Binary from current branch '$PHP_VERSION_GIT_BRANCH' on https://github.com/php/php-src"
 
